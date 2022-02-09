55d6186aa4e6740090fae3e717c9ed12366205c3
Zain Abidin <xan.abdn@gmail.com>
2022-02-09 18:25:49 +0000

    search bar with button added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const container = document.querySelector(".container");
 const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
-const container = document.querySelector(".container");
+
 let searchQuery = "";
 
 
