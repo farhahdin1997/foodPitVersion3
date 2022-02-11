0bbb04936cffab571367776dae5d28e22a27d076
Andrea Mac <aleatrengrouse@gmail.com>
2022-02-11 21:26:19 +0000

    ignore pull request

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
+//declared variables including API IDs and Keys
 const searchForm = document.querySelector("#search-bar");
 const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector(".container");
-const searchList = document.querySelector('#searchesList')
+const searchList = document.querySelector("#searchesList");
 let searchQuery = "";
 
-let searches= JSON.parse(localStorage.getItem("searches"))|| []
+let searches = JSON.parse(localStorage.getItem("searches")) || [];
 
 const APP_ID = "753e732f";
 const APP_key_recipe = "919bc78d8050b9b0caf8f5423c444aa1";
 const APP_youtube = "AIzaSyDePJu7r8npNaIknsEXLRUTajXIxst0Cf0";
 
+//submit listener for search bar and its Local Storage
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
-  
-  if (searches.indexOf(searchQuery)=== -1) {
-    searches.push(searchQuery)
-    localStorage.setItem('searches',JSON.stringify(searches))
+
+  if (searches.indexOf(searchQuery) === -1) {
+    searches.push(searchQuery);
+    localStorage.setItem("searches", JSON.stringify(searches));
   }
-  
-  searchList.innerHTML=
-searches.map(search => {
-    return `<li class="search-list">${search} </li>`
-}).join('')
-console.log(localStorage)
+  //displaying Local Storage in list format
+  searchList.innerHTML = searches
+    .map((search) => {
+      return `<li class="search-list">${search} </li>`;
+    })
+    .join("");
+  console.log(localStorage);
 
   fetchAPI(searchQuery);
 });
+//fetch function for Recipies API
 async function fetchAPI(searchQuery) {
   const baseRecipeURL = `https://api.edamam.com/api/recipes/v2?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key_recipe}&to=10&type=public`;
   const response = await fetch(baseRecipeURL);
@@ -35,26 +39,19 @@ async function fetchAPI(searchQuery) {
   generateHTML(data.hits);
   console.log(data);
 }
+//fetch function for YouTube API
 async function searchYouTube(event) {
   console.log(event.target.dataset.source);
   const baseYouTubeURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${event.target.dataset.source}&type=video&key=${APP_youtube}`;
-  console.log(baseYouTubeURL);
   const response = await fetch(baseYouTubeURL);
-
+  //looking at fetched data and saving in variables to be used for HTML
   const data = await response.json();
-  console.log(data);
   const videoId = data.items[0].id.videoId;
   const url = `https://youtube.com/watch/${videoId}`;
   $(".relative-video").attr("href", `https://youtube.com/watch/${videoId}`);
   console.log(url);
 }
-
-
-// let searches= JSON.parse(localStorage.getItem("searches"))|| []
-
-
-
-
+//using fetched data from both APIs and populating inside HTML
 function generateHTML(results) {
   console.log(results);
   let generatedHTML = "";
@@ -70,14 +67,10 @@ function generateHTML(results) {
        </div>
 
        <div >
-          <a class="view-button" href="${
-            result.recipe.url
-          }"target="_blank">View Recipe</a> <br>
+          <a class="view-button" href="${result.recipe.url}"target="_blank">View Recipe</a> <br>
 
     
-          <a class="relative-video" data-source="${
-            result.recipe.label
-          }" href="#" >Relative Video</a>
+          <a class="relative-video" data-source="${result.recipe.label}" href="#" >Relative Video</a>
        </div>
        </div>
        </container>
@@ -89,16 +82,3 @@ function generateHTML(results) {
   // open a new link on youtube when the button is clicked for video search
   $(".relative-video").click(searchYouTube);
 }
-
-
-// async function searchYouTube(searchQuery){
-//   const baseYouTubeURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&type=video&key=AIzaSyDePJu7r8npNaIknsEXLRUTajXIxst0Cf0`;
-//   const response = await fetch(baseYouTubeURL);
-//   console.log(response);
-//   const data = await response.json();
-
-//   console.log(data)
-// }
-
-
-
